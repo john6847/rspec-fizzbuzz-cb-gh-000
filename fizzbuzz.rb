0412e9593bfcdb4edcf0b022386c28528a626625
John Bien-aime <20131256@ce.pucmm.edu.do>
2019-05-25 03:01:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
+  if number % 3
+    "Fizz"
+  elsif number % 5
+    "Buzz"
+  elsif number % 3 && number % 5
+    "FizzBuzz"
+  end
 end
